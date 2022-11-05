c96cd05ca1cb20b436a910e258c27712c27c9a75
ChiHoLau888 <lauchiho7@gmail.com>
2022-11-05 23:19:50 +0000

    music app update

--- src/CW1/MusicApp.java
+++ src/CW1/MusicApp.java
@@ -1,5 +1,47 @@
 package CW1;
+import java.util.ArrayList;
+import helpers.*;
 
-public class MusicApp{
+public class MusicApp
+{
+  private ArrayList<Song> songs;
+
+  public MusicApp()
+  {
+    songs = new ArrayList<Song>();
+    addSongs();
+    doMainMenu();
+  }
+
+  public void doMainMenu()
+  {
+    boolean wantToQuit = false;
+
+    while(!wantToQuit)
+    {
+      displayMenu();
+      int choice = InputReader.getInt("Please enter your song choice > ");
+
+      switch(choice)
+      {
+        case 1: addSong();break;
+        case 2: deleteSong();break;
+        case 3: printSongs(0);break;
+        case 4: printTopSongs();break;
+        case 5: wantToQuit = true; break;
+      }
+    }
+  }
+
+  public void displayMenu()
+  {
+    System.out.println("\n 1. Add Song");
+    System.out.println(" 2. Delete Song");
+    System.out.println(" 3. Print All Songs");
+    System.out.println(" 4. Print Top Songs");
+    System.out.println(" 5. Quit\n");
+  }
+
+  private void
 
 }
--- src/CW1/Song.java
+++ src/CW1/Song.java
@@ -1,5 +1,34 @@
 package CW1;
 
-public class Song {
-    
+public class Song 
+{
+
+    private String title;
+
+    private String artist;
+
+    private int timesPlayed;
+
+    public Song(String title, String name, int count)
+    {
+        this.title = title;
+        this.artist = name;
+        this.timesPlayed = count;
+    }
+
+    public void print()
+    {
+        System.out.println(title + " by " + artist + " - " + timesPlayed);
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public int getTimesPlayed()
+    {
+        return timesPlayed;
+    }
+
 }
