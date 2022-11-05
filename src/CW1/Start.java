987d60e8c3ea794d7207de7e525eb0cf9d00045b
ChiHoLau888 <lauchiho7@gmail.com>
2022-11-05 22:09:56 +0000

    add print start

---
+++ src/CW1/Start.java
@@ -0,0 +1,16 @@
+package CW1;
+
+public class Start 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("==================================");
+        System.out.println("CO452 Programming Concepts 2022/23");
+        System.out.println("==================================");
+        System.out.println();
+        System.out.println("Music App (CW1)");
+        System.out.println();
+        System.out.println("By Chi Ho Lau");
+        System.out.println();
+    }
+}
